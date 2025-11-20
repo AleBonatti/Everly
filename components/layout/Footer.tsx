ce666023e6510750f909c53b8276602d86d70549
AleBonatti <la.seppia@gmail.com>
2025-11-20 14:29:00 +0100

    footer update

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -6,10 +6,18 @@ export default function Footer() {
   return (
     <footer className="w-full">
       <Container size="2xl" className="mb-4">
-        <div className="rounded-2xl bg-[#F2F2F2] dark:border-neutral-800 dark:bg-neutral-900">
-          <div className="flex h-16 items-center justify-center">
-            <p className="text-sm text-neutral-600 dark:text-neutral-400">
+        <div className="px-4 sm:px-6 lg:px-8 rounded-2xl bg-[#F2F2F2] dark:border-neutral-800 dark:bg-neutral-900">
+          <div className="flex justify-between items-center h-16">
+            <p className="text-base font-medium text-secondary dark:text-neutral-400">
               © {currentYear} FutureList. All rights reserved.
+            </p>
+            <p className="text-base font-light space-x-6 text-secondary dark:text-neutral-400">
+              <a href="#" className="hover:underline">
+                Privacy policy
+              </a>
+              <a href="#" className="hover:underline">
+                Cookie policy
+              </a>
             </p>
           </div>
         </div>
